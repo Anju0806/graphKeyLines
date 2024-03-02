64b6c84a5881696c59deeb146dda0d3830b9d91c
anju <anjualfino@gmail.com>
2024-03-03 04:05:15 +0800

    adding neo4j config file

--- src/neo4jConfig.js
+++ src/neo4jConfig.js
@@ -1,8 +1,10 @@
-// neo4jConfig.js
-require('dotenv').config(); // Load environment variables from .env file
-const neo4j = require('neo4j-driver');
-
-const driver = neo4j.driver('bolt://localhost:7690', neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD));
-const session = driver.session();
-
-module.exports = session;
+ // neo4jConfig.js
+ require('dotenv').config(); // Load environment variables from .env file
+ const neo4j = require('neo4j-driver');
+ 
+ const driver = neo4j.driver('bolt://localhost:7690', neo4j.auth.basic('neo4j','123anjushaji'));
+ const session = driver.session();
+ 
+ module.exports = session;
+  
+ 
