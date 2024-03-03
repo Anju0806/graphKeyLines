bfdae4ebe47d3fecf4e8da987a246627a31f84b0
anju <anjualfino@gmail.com>
2024-03-04 00:37:24 +0800

    local host issue resolved

--- src/neo4jConfig.js
+++ src/neo4jConfig.js
@@ -2,7 +2,7 @@
  require('dotenv').config(); // Load environment variables from .env file
  const neo4j = require('neo4j-driver');
  
- const driver = neo4j.driver('bolt://localhost:7690', neo4j.auth.basic('neo4j','123anjushaji'));
+ const driver = neo4j.driver('bolt://127.0.0.1:7690', neo4j.auth.basic('neo4j','1234anjushaji'));
  const session = driver.session();
  
  module.exports = session;
